35b9375f1b03ec4e126e3a71b673d52b264ce986
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2024-12-11 20:35:12 +0100

    Added code for creating tables: Terms, Trainers

--- tables.sql
+++ tables.sql
@@ -10,3 +10,19 @@ CREATE TABLE Activities(
 	Type VARCHAR(30) NOT NULL CHECK(Type IN ('Snaga', 'Kardio', 'Yoga', 'Ples', 'Rehabilitacija')),
 	Price DECIMAL NOT NULL CHECK(Price > 0)
 )
+
+CREATE TABLE Terms(
+	Id SERIAL PRIMARY KEY,
+	IdActivity INT REFERENCES Activities(Id),
+	Date DATE NOT NULL DEFAULT CURRENT_DATE,
+	MaxParticipants INT NOT NULL CHECK(MaxParticipants > 0)
+)
+
+CREATE TABLE Trainers(
+	Id SERIAL PRIMARY KEY,
+	IdCountry VARCHAR(30) NOT NULL,
+	FirstName VARCHAR(30) NOT NULL,
+	LastName VARCHAR(30) NOT NULL,
+	BirthDate DATE NOT NULL CHECK(BirthDate < CURRENT_DATE),
+	Gender VARCHAR(30) NOT NULL CHECK(Gender IN ('Musko', 'Zensko', 'Nepoznato', 'Ostalo'))
+)
