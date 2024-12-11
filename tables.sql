f56b644b1279622f0fd713393ca699df206fd8e9
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2024-12-11 20:24:39 +0100

    Added CREATE TABLE Activities

--- tables.sql
+++ tables.sql
@@ -3,3 +3,10 @@ CREATE TABLE FitnessCenters(
 	Name VARCHAR(30) NOT NULL,
 	WorkingTime VARCHAR(30)
 )
+
+CREATE TABLE Activities(
+	Id SERIAL PRIMARY KEY,
+	IdFitnessCenter INT REFERENCES FitnessCenters(Id),
+	Type VARCHAR(30) NOT NULL CHECK(Type IN ('Snaga', 'Kardio', 'Yoga', 'Ples', 'Rehabilitacija')),
+	Price DECIMAL NOT NULL CHECK(Price > 0)
+)
